35b669df16c5adc6c2290913ad2ac0110d7dc0cb
Sveta Vansovich <yansovich.ma@gmail.com>
2024-07-05 16:47:00 +0300

    add drop

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const clearBtn = document.getElementById('clear')
 const enterBtn = document.getElementById('enter')
 const backspaceBtn = document.getElementById('backspace')
 
+// калькулятор 
 for (let i = 0; i < digits.length; i++) {
     digits[i].addEventListener('click', function(event) {
         screen.value += event.target.textContent;
@@ -18,12 +19,28 @@ function enterValue () {
     console.log(screen.value);
 }
 
-function deleteLastymbol (value) {
-    // num = screen.value
-    const v  = screen.value.slice(0, -1);
-    console.log(v);
+function deleteLastymbol () {
+    const str = screen.value.slice(0, -1);
+    // console.log(str)
+    screen.value = str
 }
 
 clearBtn.addEventListener('click', clearScreen)
 enterBtn.addEventListener('click', enterValue)
-backspaceBtn.addEventListener('click', deleteLastymbol('v'))
+backspaceBtn.addEventListener('click', deleteLastymbol)
+
+//капли 
+const fieldGame = document.querySelector('.field-game')
+const btnStartGame = document.querySelector('.btn-start')
+
+function createDrops() {
+    const drop = document.createElement('div');
+    drop.classList.add('drop');
+
+    const {width, height} = fieldGame.getBoundingClientRect()
+    console.log(width, height);
+
+    fieldGame.append(drop);
+}
+
+createDrops()
