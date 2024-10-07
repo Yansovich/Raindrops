a096280e7d6cc98f65a1776a9a2acdd5053e1da6
Sveta Vansovich <yansovich.ma@gmail.com>
2024-10-07 23:41:22 +0300

    fix7

--- main.js
+++ main.js
@@ -5,29 +5,33 @@ const enterBtn = document.getElementById('enter')
 const backspaceBtn = document.getElementById('backspace')
 const scoreGame = document.querySelector('.result-value')
 
+const homePage = document.querySelector('.home-page')
+const gamePage = document.querySelector('.container-game')
+const finalPage = document.querySelector('.final-page')
+
 // калькулятор
 for (let i = 0; i < digits.length; i++) {
-    digits[i].addEventListener('click', function(event) {
-        screen.value += event.target.textContent; 
+    digits[i].addEventListener('click', function (event) {
+        screen.value += event.target.textContent;
     })
 }
 
-let result = ''
-let score = 0
+let result = '';
+let score = 0;
 const drop = document.createElement('div');
-const arrSigns = ['+', '-']
+const arrSigns = ['+', '-', '*', '/']
 
-function clearScreen () {
+function clearScreen() {
     screen.value = ''
 }
 
-function enterValue () {
+function enterValue() {
     // console.log(screen.value);
     checkRightAnswer()
     clearScreen()
 }
 
-function deleteLastSymbol () {
+function deleteLastSymbol() {
     const str = screen.value.slice(0, -1);
     screen.value = str
 }
@@ -37,133 +41,168 @@ enterBtn.addEventListener('click', enterValue)
 backspaceBtn.addEventListener('click', deleteLastSymbol)
 
 //капли 
-const fieldGame = document.querySelector('.field-game')
+// const fieldGame = document.querySelector('.field-game')
 const btnStartGame = document.querySelector('.btn-start')
 const fieldCalc = document.querySelector('.container-calc')
 const containerNums = document.getElementById('container-nums')
-
-function checkRightAnswer () {
-
-    drop.result = result  
-    
-    if (+result === +screen.value) {
-        score = score + 1      
-        scoreGame.textContent = score 
-        deleteDrops()
-        winSound()
-
-        createDrops()  /// ????    
-        insertDrops()
-        
-    } else {
-        mistakeSound()
-        growWave()
-    }
-}
 
 // создать капли
 function createDrops() {
     drop.classList.add('drop');
     const firstNum = document.createElement('div');
     const secondNum = document.createElement('div');
-    const operation = document.createElement('div');
+    const operator = document.createElement('div');
 
-    firstNum.innerText = getRandomIntInclusive (1, 10)
-    secondNum.innerText = getRandomIntInclusive (1, 10)
-    operation.innerText = getRandomSign()
-    
-    switch (operation.innerText) {
-        case '+': result = +firstNum.innerText + +secondNum.innerText 
-        break
-        case '-': result = +firstNum.innerText - +secondNum.innerText 
-        break
+    firstNum.innerText = getRandomIntInclusive(1, 10)
+    secondNum.innerText = getRandomIntInclusive(1, 10)
+    operator.innerText = getRandomSign()
+
+    if (+firstNum.innerText < +secondNum.innerText && (operator.innerText === '-' || operator.innerText === '/')) {
+        [firstNum.innerText, secondNum.innerText] = [secondNum.innerText, firstNum.innerText]
     }
-    
+
+    if (operator.innerText === '/' && +firstNum.innerText % +secondNum.innerText != 0) {
+        firstNum.innerText -= firstNum.innerText % secondNum.innerText
+    }
+
+    switch (operator.innerText) {
+        case '+': result = +firstNum.innerText + +secondNum.innerText
+            break
+        case '-': result = +firstNum.innerText - +secondNum.innerText
+            break
+        case '*': result = +firstNum.innerText * +secondNum.innerText
+            break
+        case '/': result = +firstNum.innerText / +secondNum.innerText
+            break
+    }
+
     drop.result = result
-   
+
     drop.appendChild(firstNum)
-    drop.appendChild(operation)
+    drop.appendChild(operator)
     drop.appendChild(secondNum)
 
     return drop
 }
 
 // вставить капли
-function insertDrops () {
-    fieldGame.appendChild(drop)
+function insertDrops() {
+    gamePage.appendChild(drop)
     const dropWidth = drop.offsetWidth
-    const areaWidth = fieldGame.offsetWidth
-    const maxLeftPosition  = parseInt(100 - dropWidth / areaWidth * 100)
+    const areaWidth = gamePage.offsetWidth
+    const maxLeftPosition = parseInt(100 - dropWidth / areaWidth * 100)
     drop.style.left = getRandomIntInclusive(0, maxLeftPosition) + '%'
 }
 
 function deleteDrops() {
-    drop.classList.add('hide');
+    // drop.classList.add('hide');
+    drop.remove()
+}
+
+function showDrops() {
+    drop.classList.add('show');
+}
+
+let trueResult = 0;
+let wrongResult = 0;
+const resultGame = document.querySelector('.result-game')
+
+// проверять рез
+const sectionGame = document.querySelector('.game-page')
+
+function checkRightAnswer() {
+
+    drop.result = result
+
+    if (+result === +screen.value) {
+        score = score + 10;
+        trueResult = trueResult + 1;
+        scoreGame.textContent = score
+        resultGame.textContent = score
+        deleteDrops()
+        winSound()
+
+        // console.log(1000000);   
+        const drop = document.createElement('div');     
+        createDrops()  /// ????   
+
+        // console.log(createDrops()); 
+        insertDrops()
+        // showDrops()
+
+    } else {
+        score = score - 5
+        wrongResult = wrongResult + 1;
+        scoreGame.textContent = score
+        resultGame.textContent = score
+
+        if (score < 0) {
+            score = 0
+            scoreGame.textContent = 0
+            resultGame.textContent = 0
+        }
+        mistakeSound()
+        growWave()
+
+        if (+wrongResult === 3) {
+            callModal()
+            sectionGame.classList.add('up');
+        }
+    }
+}
+
+function callModal() {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    gamePage.appendChild(modal)
+    console.log(modal);
+
 }
 
 // рандом знак
-function getRandomSign () {
+function getRandomSign() {
     const randomItem = arrSigns[Math.floor(Math.random() * arrSigns.length)];
     return randomItem
 }
 
 // рандом число
-function getRandomIntInclusive (min, max) {
+function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
-}
-
-// менять числа
-function randomExpression () {
-    let firstNum = getRandomIntInclusive (1, 10)
-    let secondNum = getRandomIntInclusive (1, 10)
-    let operator = ''
-
-    if (firstNum < secondNum && (operator === '-' || operator === '/')) {
-        [firstNum, secondNum] = [secondNum, firstNum]
-    }
-    if (operator === '/' && firstNum % secondNum !=0) {
-        firstNum -= firstNum % secondNum
-    }
-
-    return (firstNum, secondNum, operator)
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
 }
 
 // старт
 btnStartGame.addEventListener('click', startGame)
 
 // го
-function startGame () {
-    btnStartGame.style.display = "none"
+function startGame() {
+    homePage.classList.add('up');
     // timerGame()
     createDrops()
     insertDrops()
-}
-
-// таймер
-function timerGame () {
-    const timer = document.createElement('div');
-    timer.classList.add('timer');
-    let time = 10; 
-    const startGameTimer  = setInterval (() => {console.log('start'),
-    timer.textContent = time <=0 ? clearInterval(timer) : time--}, 1000);
-    fieldCalc.append(timer);
-    // timer.classList.remove('timer');
+    // backgroundSound()
 }
 
 // музон 
-function mistakeSound () {
+function backgroundSound() {
     const audio = new Audio();
     audio.preload = 'auto';
-    audio.src = './msc/mistake.mp3';
+    audio.src = './assets/audio/backgroundMusic.mp3';
     audio.play();
 }
 
-function winSound () {
+function mistakeSound() {
     const audio = new Audio();
     audio.preload = 'auto';
-    audio.src = './msc/win.mp3';
+    audio.src = './assets/audio/mistake.mp3';
+    audio.play();
+}
+
+function winSound() {
+    const audio = new Audio();
+    audio.preload = 'auto';
+    audio.src = './assets/audio/win.mp3';
     audio.play();
 }
 
@@ -171,24 +210,57 @@ function winSound () {
 const firstWave = document.querySelector('.field-wave')
 const secondWave = document.querySelector('.field-wave-2')
 
-function growWave () {
-  const waveHeight = firstWave.offsetHeight
-  const newHeight = waveHeight + 10
+function growWave() {
+    const waveHeight = firstWave.offsetHeight
+    const newHeight = waveHeight + 10
 
-  firstWave.style.height = newHeight + 'px'
-  secondWave.style.height = newHeight + 'px'
+    firstWave.style.height = newHeight + 'px'
+    secondWave.style.height = newHeight + 'px'
 }
 
+// домой
+const btnRestart = document.querySelector('.btn-restart')
+
+btnRestart.addEventListener('click', goToHomePage)
+
+function goToHomePage() {
+    sectionGame.style.display = "none"
+    finalPage.style.display = "none"
+    homePage.classList.remove('up');
+    sectionGame.classList.remove('up');
+
+}
+
+
+
+
+
+
+
+// не работает
 function deleteHiddenDrop() {
-    const dropHeight = drop.clientHeight
-    const waveHeight = firstWave.clientHeight 
-    const fieldGameHeight = fieldGame.clientHeight - waveHeight
-    const topDrop = fieldGameHeight - dropHeight
-    
-    if(topDrop === fieldGame) {
+    const positionDrop = drop.getBoundingClientRect()
+    const positionWave = firstWave.getBoundingClientRect()
+
+    console.log(positionDrop);
+    console.log(positionWave);
+
+    if (positionDrop.y + positionDrop.height >= positionWave.y) {
         deleteDrops()
-        createDrops()       // ????
-        insertDrops()
+        createDrops()
     }
 }
 deleteHiddenDrop()
+
+// таймер
+function timerGame() {
+    const timer = document.createElement('div');
+    timer.classList.add('timer');
+    let time = 10;
+    const startGameTimer = setInterval(() => {
+        console.log('start'),
+        timer.textContent = time <= 0 ? clearInterval(timer) : time--
+    }, 1000);
+    fieldCalc.append(timer);
+    timer.classList.remove('timer');
+}
